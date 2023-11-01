2a1357115c4e032f96e04607fae1f07bcb38160c
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2023-11-01 20:34:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,81 +5,226 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ginga</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <form action="">
-        <div id="box-1" style="display: block;">
-            <p>1</p>
+        <div id="box-1">
+            <h1>Olá</h1>
+            <p>Você certamente já se divertiu com algum jogo, seja na escola, na rua ou online!! Os jogos podem nos trazer bem-estar, melhorar nossa atenção, a interação social ou despertar nossa vontade de competir!!
+            <br>
+            E podem também nos ajudar a aprender algum tema novo de forma mais dinâmica.</p>
         </div>
-        <div id="box-2" style="display: none;">
-            <p>2</p>
+        <div id="box-2">
+            <p>O jogo que você vai começar tem por objetivo ajudar os participantes a compreender
+                sobre o conceito de identidade étnico-racial e como através desse conhecimento
+                podemos:
+                
+                Aprender mais sobre nossas origens
+                
+                Melhorar a forma como nos vemos e nos sentimos em relação a nós mesmos
+                
+                Conhecer a diversidade existente ao nosso redor
+                
+                Ser mais tolerantes e atentos para o combate à discriminação</p>
         </div>
-        <div id="box-3" style="display: none;">
-            <p>3</p>
+        <div id="box-3">
+            <p>Você sabe o que é identidade étnico-racial?</p>
         </div>
-        <div id="box-4" style="display: none;">
-            <p>4</p>
+        <div id="box-4">
+            <p>Como você sabe, todo jogo tem um nome. Esse jogo que você vai iniciar se chama...</p>
+            <h1>GINGA</h1>
         </div>
-        <div id="box-5" style="display: none;">
-            <p>sadvfgbn4h</p>
+        <div id="box-5">
+            <p>Você vai aprender sobre o significado do Ginga mais a frente.
+
+                Enquanto isso, vamos conhecer um pouco sobre as regras desse jogo?
+                
+                O jogo é composto por 6 etapas e vai acontecer em 6 encontros.
+                
+                Nós iremos desenvolver algumas questões em encontros
+                presenciais e você terá algumas tarefas para desenvolver em casa.
+                
+                Para responder às questões você receberá orientações do
+                responsável pela atividade. Também terá a possibilidade de acessar
+                vídeos e músicas de apoio.</p>
         </div>
-        <div id="box-6" style="display: none;">
-            <p>sadvf5gbnh</p>
+        <div id="box-6">
+            <p>Vamos conhecer as etapas do jogo:
+                **criar nomes mais dinâmicos e menos teóricos
+                
+                QUE HISTÓRIA É ESSA QUE ME CONTARAM?(CONSCIÊNCIA HISTÓRICA)
+                
+                BRANQUITUDE
+                
+                EXERCÍCIO DA NEGRITUDE
+                
+                COMBATE AO RACISMO
+                
+                CULTURA AFROBRASILEIRA
+                
+                TURISMO AFROCENTARDO
+                
+                Cada uma dessas etapas serão aprendidas por meio de atividades.
+                Quanto mais atividades você concluir, maior será sua pontuação.</p>
         </div>
-        <div id="box-7" style="display: none;">
-            <p>sadv7fgbnh</p>
+        <div id="box-7">
+            <p>Vamos começar!!
+
+                Você já ouviu falar em diversidade? Essa palavra nos ajuda a
+                compreender que existem muitas diferenças no mundo. Você
+                certamente as conhece: temos as diferenças de gênero, raça, etnia,
+                condição social e econômica, religião, no jeito de pensar, no jeito de se
+                divertir. E muitas outras!!</p>
             <textarea name="" id="" cols="30" rows="10"></textarea>
         </div>
-        <div id="box-8" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-8">
+            <p>Compreender a existência das diferenças pode gerar muitos impactos
+                positivos. Mas quando isso não acontece, as consequências podem ser
+                muito negativas. O racismo é um exemplo.
+                
+                Escreva aqui o que você pensa sobre isso:</p>
         </div>
-        <div id="box-9" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-9">
+            <p>Aprender sobre a verdadeira história por trás do
+                racismo é uma forma de combate sabia?
+                Aliás tem uma autora que diz que “para pensar soluções para uma realidade,
+                devemos tirá-la da invisibilidade”. Essa autora se chama Djamila Ribeiro e tem
+                muito para nos ensinar. Você pode conhecer sobre ela clicando aqui:</p>
         </div>
-        <div id="box-10" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-10">
+            <p>Você certamente já ouviu ou até mesmo já disse frases como:
+                Somos todos iguais
+                Não existem diferenças entre as pessoas
+                Basta você se esforçar que você consegue
+                As oportunidades são iguais para todos</p>
         </div>
-        <div id="box-11" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-11">
+            <p>Mas o que você precisa saber é que a história
+                não é bem assim.
+                
+                Vídeo sobre racismo estrutural ( assistir o vídeo ganha 20 pontos)</p>
         </div>
-        <div id="box-12" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-12">
+            <div> <h2>1</h2>O conceito de raça não faz parte da Biologia. Aliás você sabia que biologicamente não existem diferenças significativas para classificar as pessoas como brancas ou negras?</div>
         </div>
-        <div id="box-13" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-13">
+            <div> <h2>2</h2>Na verdade, o conceito de raça foi usado para
+                inferiorizar o povo negro e dessa forma serem
+                dominados mais facilmente.</div>
         </div>
-        <div id="box-14" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-14">
+            <div> <h2>3</h2>E você sabe como isso aconteceu? Através do processo de colonização. Você já estudou sobre esse tema na escola?</div>
+
+            <button>Sim</button>
+            <button>Não</button>
+            <button>Não Lembro</button>
         </div>
-        <div id="box-15" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-15">
+            <p>Escreva um pouco sobre o que sabe a respeito de colonização</p>
         </div>
-        <div id="box-16" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-16">
+            <p>Você certamente gosta de algumas coisas, como ouvir certas musicas, frequentar alguns lugares, estar com seus amigos ou familiares, se vestir de uma determinada forma ou ainda ter uma crença religiosa.</p>
         </div>
-        <div id="box-17" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-17">
+            <p>Agora imagine: você sendo retirado (a) dessa realidade sem ao menos ter a
+                possibilidade de escolha. Isso mesmo: de uma hora pra outra. É brutalmente
+                retirado dos seus costumes, violentado e silenciado.</p>
         </div>
-        <div id="box-18" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-18">
+            <p>Depois de ser separado da sua história, viaja por
+                meses para um local que você não faz ideia de
+                como será: e se chegará vivo até la. Nessa viagem,
+                as condições de alimentação e higiene são
+                precárias. E ainda: infelizmente algumas pessoas
+                não resistem.</p>
+            <p>O resultado desse processo foi a
+                desumanização de um povo: o povo
+                negro.
+                
+                Dessa forma, o sistema de dominação
+                estava pronto para acontecer. E
+                acontece até os dias de hoje através do
+                silenciamento.</p>
         </div>
-        <div id="box-19" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-19">
+            <p>Essa mulher, de nome oficial desconhecido, nasceu na
+                Angola e foi escravizada por uma família portuguesa na
+                Bahia no século XVIII. Recebeu o nome de Anastácia.
+                Sabe porque essa máscara em seu rosto?
+                
+                Ela tentava ajudar outros escravizados a fugir
+                
+                Se recusava a manter relações sexuais com o
+                “senhor” de escravos
+                
+                Diziam que ela possuía poderes de cura e que
+                chegou a realizar milagres.
+                
+                Há relatos que a prática de cobrir o rosto dos
+                escravizados também era uma tentativa de impedir que
+                cometessem suicídio comendo terra.</p>
         </div>
-        <div id="box-20" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-20">
+            <p>O racismo acontece dessa forma: através da
+                dominação. Isso é possível pois algumas pessoas
+                ocupam um lugar de poder, enquanto outras são
+                dominadas. A depender do grupo racial que se faz
+                parte, isso poderá gerar privilégios ou desvantagens.
+                Por isso não existe racismo reverso. O lugar de quem
+                domina e daquele que é dominado foi estruturado na
+                sociedade.</p>
         </div>
-        <div id="box-21" style="display: none;">
-            <p>sadvfgbnh</p>
+        <div id="box-21">
+            <p>E quais são as consequências que
+                observamos até os dias atuais?
+                
+                https://www.youtube.com/watch?v=bgJ
+                4AsXXsSM</p>
         </div>
-        <div id="box-22" style="display: none;">
-            <p>sadvfgbnh1</p>
+        <div id="box-22">
+            <p>Parabéns por ter concluído essa primeira etapa. Você terá algumas
+                atividades vão ajudar na sua pontuação. Tire alguns minutinhos e
+                conclua essa fase.</p>
+
+            <p>Analise as frases a seguir, classifique como Verdadeira ou Falsa e justifique sua resposta:
+
+                Somos todos iguais ( V ) ( F )
+                
+                Existe racismo reverso. ( V ) ( F )
+                
+                A colonização aconteceu até o século 19 e atualmente não são observadas marcas na
+                sociedade ( V ) ( F )</p>
+        </div>
+        <div id="box-23">
+            <p>Conteúdo extra:
+
+                Diferença entre preconceito, discriminação e preconceito.
+                
+                Silvio Almeida é o atual Ministro dos Direitos Humanos no Brasil
+                (2022-2026). Ele escreveu um livro chamado Racismo Estrutural e traz
+                três importantes conceitos:
+                
+                Preconceito racial é o juízo baseado em estereótipos acerca de
+                indivíduos que pertençam a um determinado grupo racializado, e
+                que pode ou não resultar em práticas discriminatórias
+                
+                Discriminação racial é a atribuição de tratamento diferenciado a
+                membros de grupos racialmente identificados.
+                
+                Racismo é uma forma sistemática de discriminação que tem a raça
+                como fundamento, e se manifesta por meio de práticas conscientes
+                ou inconscientes que culminam em privilégios ou desvantagenspara
+                indivíduos, a depender do grupo racial ao qual fazem parte.</p>
+        </div>
+        <div id="box-24">
+            <p>Sua pontuação até aqui foi</p>
         </div>
         <button type="submit">Finalizar</button>
     </form>
-    <button id="botao_voltar" onclick="mudar_pagina(-1)" disabled>Mostrar anterior Caixa</button>
-    <button id="botao_passar" onclick="mudar_pagina(1)">Mostrar Próxima Caixa</button>
+    <button id="botao_voltar" onclick="mudar_pagina(-1)" disabled> < </button>
+    <button id="botao_passar" onclick="mudar_pagina(1)"> > </button>
     <script src="h.js"></script>
 </body>
 
