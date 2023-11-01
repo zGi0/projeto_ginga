6382c6282d8c464bd7300ca656239733ce956cef
raphael0627f <raphaelferreirafo@gmail.com>
2023-11-01 18:44:42 -0300

    .

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ginga</title>
+</head>
+<body>
+    <h1>Olá</h1>
+    <p>
+        Certamente você já se divertiu com algum jogo, seja na escola, na rua ou online!! Os
+        jogos podem nos trazer bem-estar, melhorar nossa atenção, a interação social ou
+        despertar nossa vontade de competir!!
+    </p>
+    <p>Podem também nos ajudar a aprender algum tema novo de forma mais dinâmica.</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ufbZkexu7E0?si=ULWw5CknX_IwbINR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <a href="#">Próximo</a>
+</body>
+</html>
